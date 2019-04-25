7729a7e30612b8584db170acea7596fac27474b6
Stepan Snigirev <snigirev.stepan@gmail.com>
2019-04-25 18:46:23 +0200

    move from micropython fork

---
+++ BSP_DISCO_F769NI/Drivers/BSP/STM32F769I-Discovery/mbed_wait_api.h
@@ -0,0 +1,8 @@
+#ifndef __WAIT_MBED_H
+#define __WAIT_MBED_H
+
+#include "stm32f7xx_hal.h"
+
+#define wait_ms HAL_Delay
+
+#endif
---
+++ micropython.mk
@@ -0,0 +1,24 @@
+EXAMPLE_MOD_DIR := $(USERMOD_DIR)
+
+# Add all C files to SRC_USERMOD.
+SRC_USERMOD += $(EXAMPLE_MOD_DIR)/BSP_DISCO_F769NI/Utilities/Fonts/font12.c
+SRC_USERMOD += $(EXAMPLE_MOD_DIR)/BSP_DISCO_F769NI/Utilities/Fonts/font16.c
+SRC_USERMOD += $(EXAMPLE_MOD_DIR)/BSP_DISCO_F769NI/Utilities/Fonts/font20.c
+SRC_USERMOD += $(EXAMPLE_MOD_DIR)/BSP_DISCO_F769NI/Utilities/Fonts/font24.c
+SRC_USERMOD += $(EXAMPLE_MOD_DIR)/BSP_DISCO_F769NI/Utilities/Fonts/font8.c
+SRC_USERMOD += $(EXAMPLE_MOD_DIR)/BSP_DISCO_F769NI/Drivers/BSP/STM32F769I-Discovery/stm32f769i_discovery.c
+SRC_USERMOD += $(EXAMPLE_MOD_DIR)/BSP_DISCO_F769NI/Drivers/BSP/STM32F769I-Discovery/stm32f769i_discovery_lcd.c
+SRC_USERMOD += $(EXAMPLE_MOD_DIR)/BSP_DISCO_F769NI/Drivers/BSP/STM32F769I-Discovery/stm32f769i_discovery_sdram.c
+SRC_USERMOD += $(EXAMPLE_MOD_DIR)/BSP_DISCO_F769NI/Drivers/BSP/Components/otm8009a/otm8009a.c
+SRC_USERMOD += $(EXAMPLE_MOD_DIR)/BSP_DISCO_F769NI/Drivers/BSP/Components/adv7533/adv7533.c
+SRC_USERMOD += $(EXAMPLE_MOD_DIR)/STM32F7xx_HAL_Driver/Src/stm32f7xx_hal_dma2d.c
+SRC_USERMOD += $(EXAMPLE_MOD_DIR)/STM32F7xx_HAL_Driver/Src/stm32f7xx_hal_ltdc.c
+SRC_USERMOD += $(EXAMPLE_MOD_DIR)/STM32F7xx_HAL_Driver/Src/stm32f7xx_hal_ltdc_ex.c
+SRC_USERMOD += $(EXAMPLE_MOD_DIR)/STM32F7xx_HAL_Driver/Src/stm32f7xx_hal_dsi.c
+SRC_USERMOD += $(EXAMPLE_MOD_DIR)/display.c
+
+# We can add our module folder to include paths if needed
+# This is not actually needed in this example.
+CFLAGS_USERMOD += -I$(EXAMPLE_MOD_DIR)/BSP_DISCO_F769NI
+CFLAGS_USERMOD += -I$(EXAMPLE_MOD_DIR)/BSP_DISCO_F769NI/Drivers/BSP/STM32F769I-Discovery
+CFLAGS_USERMOD += -I$(EXAMPLE_MOD_DIR)/STM32F7xx_HAL_Driver/Inc
